07badcce28dd652c9bf2636d6665b3080c86e739
priya <priyankpatel201@gimal.com>
2022-01-12 08:41:26 +0000

    best-buy-api serenity

---
+++ src/main/java/com/bestbuy/api/constants/EndPoint.java
@@ -0,0 +1,26 @@
+package com.bestbuy.api.constants;
+
+public class EndPoint {
+
+    public static final String GET_ALL_THE_PRODUCTS = "/products";
+    public static final String CREATE_NEW_PRODUCT = "/products";
+    public static final String UPDATE_SINGLE_PRODUCT_BY_ID = "/products/{id}";
+    public static final String DELETE_SINGLE_PRODUCT_BY_ID = "/products/{id}";
+
+   public static final String GET_ALL_THE_STORES = "/stores";
+    public static final String CREATE_NEW_STORE = "/stores";
+    public static final String UPDATE_SINGLE_STORE_BY_ID = "/store/{id}";
+    public static final String DELETE_SINGLE_STORE_BY_ID = "/store/{id}";
+
+    public static final String GET_ALL_THE_SERVICES = "/services";
+    public static final String CREATE_NEW_SERVICE = "/services";
+    public static final String UPDATE_SINGLE_SERVICE_BY_ID = "/services/{id}";
+    public static final String DELETE_SINGLE_SERVICE_BY_ID = "/services/{id}";
+
+    public static final String GET_ALL_THE_CATEGORIES = "/categories";
+    public static final String CREATE_NEW_CATEGORY = "/categories";
+    public static final String UPDATE_SINGLE_CATEGORY_BY_ID = "/categories/{id}";
+    public static final String DELETE_SINGLE_CATEGORY_BY_ID =  "/categories/{id}";
+
+}
+
